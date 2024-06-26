a72128afb005400d5b4cc2742c4940f343af33d7
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-06-26 13:44:31 -0500

    update to use sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'paul@devtogo.io',
+        defaultReplyTo: 'paul@devtogo.io',
+      },
+    },
+  },
+
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: ["nickname"],
+      },
+    },
+  },
+});
